a3c0e4bbf0f6c946c09ebdc11d6aa8c7169ae517
fellfire <r1der88@mail.ru>
2024-05-30 07:59:02 +0300

    Settings array

--- src/Root/Settings/Settings.php
+++ src/Root/Settings/Settings.php
@@ -34,7 +34,7 @@ class Settings
         }
     }
 
-    public static function get(string $file, string $name): string
+    public static function get(string $file, string $name): string | array
     {
         if (!isset(self::$settings->{$file})) {
             LogHelper::log('file not found', ['file' => $file, 'name' => $name], 'errors/');
