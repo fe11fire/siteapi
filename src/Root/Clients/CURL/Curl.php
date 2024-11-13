5d2df0c4307a0a9045bbab36454f29263d44301a
fellfire <r1der88@mail.ru>
2024-11-13 07:22:24 +0300

    Curl ping

--- src/Root/Clients/CURL/Curl.php
+++ src/Root/Clients/CURL/Curl.php
@@ -87,4 +87,20 @@ class Curl
     {
         curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode($payload));
     }
+
+    public static function ping(string $url): bool
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_NOBODY, true);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return boolval($response);
+    }
 }
