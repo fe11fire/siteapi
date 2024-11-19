72c6e6a4e164dbb37edc6bc95d5ef78ca238b4e5
fellfire <r1der88@mail.ru>
2024-11-19 07:27:17 +0300

    MessageWithInlineExample

---
+++ src/Root/Clients/Telegram/Templates/MessageWithInlineExample.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SiteApi\Root\Clients\Telegram\Templates;
+
+use SiteApi\Root\Settings\Settings;
+use SiteApi\Root\Messenger\ClientContract;
+use SiteApi\Root\Messenger\TemplateContract;
+
+
+final class MessageWithInlineExample extends TemplateContract
+{
+    function makeMessage(ClientContract $client): array
+    {
+        switch ($client->getServiceName()) {
+            case ClientContract::SERVICE_TELEGRAM:
+                return [implode(chr(10), $this->data)];
+                break;
+            default:
+                # code...
+                break;
+        }
+        return [];
+    }
+
+    public function makeReplyMarkup(ClientContract $client): array
+    {
+        $keyboard = [
+            'inline_keyboard' => [[
+                [
+                    "text" => 'Назад',
+                    "web_app" => ['url' => 'https://'],
+                ]
+            ]],
+        ];
+
+        return ['reply_markup' => json_encode($keyboard)];
+    }
+}
